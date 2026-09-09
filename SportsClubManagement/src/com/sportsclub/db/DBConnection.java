caf6f263c0efc73b98e8d9765f1fa935adca3d0d
aadhya-Sketch <aadhyagowda2003@gmail.com>
2026-09-09 15:04:27 +0530

    Update DBConnection.java

--- SportsClubManagement/src/com/sportsclub/db/DBConnection.java
+++ SportsClubManagement/src/com/sportsclub/db/DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/sports_club_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "aadhya";
+    private static final String USER = "db_user";
+    private static final String PASSWORD = "db_password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
